d019a49163dbc210af89fcdaa4fb4589afe50189
sathsarasa <sathsara@wso2telco.com>
2020-05-19 09:45:56 +0530

    Add activities

---
+++ Chapter2/Activity1/activity-02-01/welcome.php
@@ -0,0 +1,16 @@
+<?php
+ 
+$hourOfDay = date('H');
+ 
+if($hourOfDay < 12) {
+    $message = "Good Morning";
+} elseif($hourOfDay > 11 && $hourOfDay < 18) {
+    $message = "Good Afternoon";
+} elseif($hourOfDay > 17){
+    $message = "Good Evening";
+}
+ 
+echo $message;
+ 
+?>
+
