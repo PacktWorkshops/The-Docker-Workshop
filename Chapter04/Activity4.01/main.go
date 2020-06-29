d189f9bfc9ac5839c53cacfe7b829cbc14ecf4ff
prachijain1992 <57354930+prachijain1992@users.noreply.github.com>
2020-06-29 12:29:44 +0530

    Update main.go

--- Chapter04/Activity4.01/main.go
+++ Chapter04/Activity4.01/main.go
@@ -1,42 +1,41 @@
 package main
- 
+
 import (
     "net/http"
     "fmt"
     "log"
     "os"
 )
- 
+
 func main() {
- 
+
     http.HandleFunc("/", defaultHandler)
     http.HandleFunc("/contact", contactHandler)
     http.HandleFunc("/login", loginHandler)
- 
+
     port := os.Getenv("PORT")
     if port == "" {
         port = "8080"
     }
-    
+
     log.Println("Service started on port " + port)
- 
+
     err := http.ListenAndServe(":"+port, nil)
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
         return
     }
- 
+
 }
- 
+
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "<h1>Home Page</h1>")
 }
- 
+
 func contactHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "<h1>Contact Us</h1>")
 }
- 
+
 func loginHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "<h1>Login Page</h1>")
 }
-
