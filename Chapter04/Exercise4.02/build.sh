74ca31522f90e6bda55467ee1c9e526efa53bbf6
prachijain1992 <57354930+prachijain1992@users.noreply.github.com>
2020-06-29 12:18:56 +0530

    Update build.sh

--- Chapter04/Exercise4.02/build.sh
+++ Chapter04/Exercise4.02/build.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
- 
-echo "Creating welcome builder image"
- 
-docker image build -t welcome-builder:v1 -f Dockerfile.build .
- 
-docker container create --name welcome-builder-container welcome-builder:v1
- 
-docker container cp welcome-builder-container:/myapp/welcome .
- 
-docker container rm -f welcome-builder-container
- 
-echo "Creating welcome runtime image"
- 
-docker image build -t welcome-runtime:v1 .
- 
-rm welcome
 
+echo "Creating welcome builder image"
+
+docker image build -t welcome-builder:v1 -f Dockerfile.build .
+
+docker container create --name welcome-builder-container welcome-builder:v1
+
+docker container cp welcome-builder-container:/myapp/welcome .
+
+docker container rm -f welcome-builder-container
+
+echo "Creating welcome runtime image"
+
+docker image build -t welcome-runtime:v1 .
+
+rm welcome
