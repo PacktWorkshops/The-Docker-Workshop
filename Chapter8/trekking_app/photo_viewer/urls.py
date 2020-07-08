058486bd2917c60d701eec3fc430d58bbc7f713f
Engy Fouda <efoda@ieee.org>
2020-07-08 17:09:04 -0400

    Delete urls.py

--- Chapter8/trekking_app/photo_viewer/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.photo_index, name="photo_index"),
-    path("<int:pk>/", views.photo_detail, name="photo_detail"),
-    path("<country>/", views.photo_country, name="photo_country"),
-]
