7f5a7f026574ac37bf57846815dd8fc36c96e6e2
prachijain1992 <57354930+prachijain1992@users.noreply.github.com>
2020-07-16 16:25:14 +0530

    Update resource_test.sql

--- Chapter12/Activity1/resource_test.sql
+++ Chapter12/Activity1/resource_test.sql
@@ -1,4 +1,4 @@
-CREATE TABLE test_data 
+CREATE TABLE test_data
 (
     random_value NUMERIC NOT NULL,
     row1         NUMERIC NOT NULL,
@@ -6,9 +6,9 @@ CREATE TABLE test_data
 );
 
 INSERT INTO test_data
-    SELECT random_value.*, 
-    gen.* , 
-    CEIL(RANDOM()*100) 
+    SELECT random_value.*,
+    gen.* ,
+    CEIL(RANDOM()*100)
     FROM GENERATE_SERIES(1, 300) random_value,
     GENERATE_SERIES(1, 900000) gen
     WHERE gen <= random_value * 300;
