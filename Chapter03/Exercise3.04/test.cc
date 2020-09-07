19949faa72f1179678a4db8fb6db9f4888d9b02c
prachijain1992 <57354930+prachijain1992@users.noreply.github.com>
2020-09-07 16:15:11 +0530

    Update test.cc

--- Chapter03/Exercise3.04/test.cc
+++ Chapter03/Exercise3.04/test.cc
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main()
 {
-   int i;
-   for (i=1; i<=10; i++)
-   {
-       printf("%d\n", i);
-   }
-   return 0;
+    int i;
+    for (i=1; i<=10; i++)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
 }
