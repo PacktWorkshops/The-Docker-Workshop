686b6d965dfa2258a12bde5ef23cca892024aee6
vincesesto <vince_sesto@mail.com>
2020-10-17 20:32:33 +1300

    Update settings.py

--- Chapter09/Activity9.01/panoramic_trekking_app/settings.py
+++ Chapter09/Activity9.01/panoramic_trekking_app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'm0xuyn!ed7s2!+c(u+r2)$$&l4tf#def9l0bcdb5epy8u55nw7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
